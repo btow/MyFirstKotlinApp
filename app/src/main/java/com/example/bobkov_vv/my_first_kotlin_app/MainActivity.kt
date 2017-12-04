53677c585738efeea11b6a6f84356765b8ad3a17
bobkov-vv <btow@yandex.ru>
2017-12-04 17:45:17 +0500

    Initial commit

---
+++ app/src/main/java/com/example/bobkov_vv/my_first_kotlin_app/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bobkov_vv.my_first_kotlin_app
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
